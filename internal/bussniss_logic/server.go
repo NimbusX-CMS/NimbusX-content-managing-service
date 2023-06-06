4ab387d7bd1b8ee8a0da931d9b3a1648dd924b14
jonas-be <jonasbender2005@gmail.com>
2023-06-06 23:48:17 +0200

    Implement user

--- internal/bussniss_logic/server.go
+++ internal/bussniss_logic/server.go
@@ -2,6 +2,7 @@ package bussniss_logic
 
 import (
 	"github.com/NimbusX-CMS/NimbusX-content-managing-service/internal/db"
+	"github.com/NimbusX-CMS/NimbusX-content-managing-service/internal/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -43,27 +44,70 @@ func (s *Server) GetSpaces(c *gin.Context) {
 }
 
 func (s *Server) PostUser(c *gin.Context) {
-	c.AbortWithStatus(http.StatusNotImplemented)
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	createdUser, err := s.DB.CreateUser(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, createdUser)
 }
 
 func (s *Server) DeleteUserUserId(c *gin.Context, userId int) {
-	c.AbortWithStatus(http.StatusNotImplemented)
+	err := s.DB.DeleteUser(userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusOK)
 }
 
 func (s *Server) GetUserUserId(c *gin.Context, userId int) {
-	c.AbortWithStatus(http.StatusNotImplemented)
+	user, err := s.DB.GetUser(userId)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 func (s *Server) PutUserUserId(c *gin.Context, userId int) {
-	c.AbortWithStatus(http.StatusNotImplemented)
-}
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-func (s *Server) GetUserUserIdSpaces(c *gin.Context, userId int) {
-	//TODO implement me
-	panic("implement me")
+	user.ID = userId
+
+	updatedUser, err := s.DB.UpdateUser(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, updatedUser)
 }
 
 func (s *Server) GetUsers(c *gin.Context) {
+	users, err := s.DB.GetUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, users)
+}
+
+func (s *Server) GetUserUserIdSpaces(c *gin.Context, userId int) {
 	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
