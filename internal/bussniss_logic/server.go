fc9abcd5c2f680dbcd100e882330c579feb01217
jonas-be <jonasbender2005@gmail.com>
2023-06-06 13:23:53 +0200

    Replace panic with http 501 Not Implemented

--- internal/bussniss_logic/server.go
+++ internal/bussniss_logic/server.go
@@ -2,86 +2,71 @@ package bussniss_logic
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type Server struct{}
 
 func (s *Server) GetLogin(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) PostLogin(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) PostPasswordToken(c *gin.Context, token string) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) PostSpace(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) DeleteSpaceSpaceId(c *gin.Context, spaceId int) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) GetSpaceSpaceId(c *gin.Context, spaceId int) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) PutSpaceSpaceId(c *gin.Context, spaceId int) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) GetSpaces(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) PostUser(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) DeleteUserUserId(c *gin.Context, userId int) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) GetUserUserId(c *gin.Context, userId int) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) PutUserUserId(c *gin.Context, userId int) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) GetUsers(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) GetWebhooks(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) PostWebhooks(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (s *Server) PutWebhooksName(c *gin.Context, name string) {
-	//TODO implement me
-	panic("implement me")
+	c.AbortWithStatus(http.StatusNotImplemented)
 }
