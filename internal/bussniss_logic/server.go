c8cf2e73303c78f4452f5951cbe8f5da423bac8e
Jonas Bender <90411737+jonas-be@users.noreply.github.com>
2023-06-07 15:08:46 +0200

    Openapi code generation (#1)

---
+++ internal/bussniss_logic/server.go
@@ -0,0 +1,76 @@
+package bussniss_logic
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Server struct{}
+
+func (s *Server) GetLogin(c *gin.Context) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) PostLogin(c *gin.Context) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) PostPasswordToken(c *gin.Context, token string) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) PostSpace(c *gin.Context) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) DeleteSpaceSpaceId(c *gin.Context, spaceId int) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) GetSpaceSpaceId(c *gin.Context, spaceId int) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) PutSpaceSpaceId(c *gin.Context, spaceId int) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) GetSpaces(c *gin.Context) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) PostUser(c *gin.Context) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) DeleteUserUserId(c *gin.Context, userId int) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) GetUserUserId(c *gin.Context, userId int) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) PutUserUserId(c *gin.Context, userId int) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) GetUserUserIdSpaces(c *gin.Context, userId int) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) GetUsers(c *gin.Context) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) GetWebhooks(c *gin.Context) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) PostWebhooks(c *gin.Context) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (s *Server) PutWebhooksName(c *gin.Context, name string) {
+	c.AbortWithStatus(http.StatusNotImplemented)
+}
