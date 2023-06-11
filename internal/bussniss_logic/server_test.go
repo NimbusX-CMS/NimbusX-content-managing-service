3218a4a40eac8986a988290112c7f469f37c625c
jonas-be <jonasbender2005@gmail.com>
2023-06-11 23:12:46 +0200

    Implement space endpoints tests

--- internal/bussniss_logic/server_test.go
+++ internal/bussniss_logic/server_test.go
@@ -8,6 +8,112 @@ import (
 	"testing"
 )
 
+func TestServer_PostSpace(t *testing.T) {
+	server := setupServer(t)
+
+	tests := TestCases{
+		{
+			name:               "PostSpace",
+			Url:                "/spaces",
+			RequestBody:        `{"name":"Test Space"}`,
+			ResponseModel:      &models.Space{},
+			ExpectedBody:       &models.Space{ID: 1, Name: "Test Space"},
+			ExpectedStatusCode: http.StatusCreated,
+		},
+	}
+	tests.testStaticUrlCases(t, server.PostSpace)
+}
+
+func TestServer_DeleteSpaceSpaceId(t *testing.T) {
+	server := setupServer(t)
+	space, err := server.DB.CreateSpace(models.Space{Name: "Test Space"})
+	if err != nil {
+		t.Error("Cannot create sample space", err)
+	}
+
+	tests := TestCases{
+		{
+			name:               "DeleteSpaceSpaceId",
+			Url:                fmt.Sprintf("/spaces/%v", space.ID),
+			ID:                 space.ID,
+			ExpectedStatusCode: http.StatusOK,
+		},
+	}
+	tests.testDynamicIntUrlCases(t, server.DeleteSpaceSpaceId)
+}
+
+func TestServer_GetSpaceSpaceId(t *testing.T) {
+	server := setupServer(t)
+	space, err := server.DB.CreateSpace(models.Space{ID: 1, Name: "Test Space"})
+	if err != nil {
+		t.Error("Cannot create sample space", err)
+	}
+	tests := TestCases{
+		{
+			name:               "GetSpaceSpaceId",
+			Url:                fmt.Sprintf("/spaces/%v", space.ID),
+			ID:                 space.ID,
+			ResponseModel:      &models.Space{},
+			ExpectedBody:       &models.Space{ID: space.ID, Name: "Test Space", Languages: []models.Language{}},
+			ExpectedStatusCode: http.StatusOK,
+		},
+	}
+
+	tests.testDynamicIntUrlCases(t, server.GetSpaceSpaceId)
+}
+
+func TestServer_PutSpaceSpaceId(t *testing.T) {
+	server := setupServer(t)
+	space, err := server.DB.CreateSpace(models.Space{ID: 1, Name: "Test Space"})
+	if err != nil {
+		t.Error("Cannot create sample space", err)
+	}
+
+	tests := TestCases{
+		{
+			name:               "PutSpaceSpaceId",
+			Url:                fmt.Sprintf("/spaces/%v", space.ID),
+			ID:                 space.ID,
+			RequestBody:        `{"name":"Updated Space"}`,
+			ResponseModel:      &models.Space{},
+			ExpectedBody:       &models.Space{ID: space.ID, Name: "Updated Space"},
+			ExpectedStatusCode: http.StatusOK,
+		},
+	}
+	tests.testDynamicIntUrlCases(t, server.PutSpaceSpaceId)
+}
+
+func TestServer_GetSpaces(t *testing.T) {
+	server := setupServer(t)
+
+	tests := TestCases{
+		{
+			name:               "GetSpaces",
+			Url:                "/spaces",
+			ResponseModel:      &[]models.Space{},
+			ExpectedBody:       &[]models.Space{},
+			ExpectedStatusCode: http.StatusOK,
+		},
+	}
+	tests.testStaticUrlCases(t, server.GetSpaces)
+}
+
+func TestServer_GetUserUserIdSpaces(t *testing.T) {
+	server := setupServer(t)
+
+	tests := TestCases{
+		{
+			name:               "GetUserUserIdSpaces",
+			Url:                "/users/1/spaces",
+			ID:                 1,
+			ResponseModel:      &[]models.SpaceAccess{},
+			ExpectedBody:       &[]models.SpaceAccess{},
+			ExpectedStatusCode: http.StatusOK,
+		},
+	}
+	tests.testDynamicIntUrlCases(t, server.GetUserUserIdSpaces)
+}
+
 func TestPostUser(t *testing.T) {
 	server := setupServer(t)
 
